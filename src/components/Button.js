53173413e7e802e69e3bf78699bea15d3907d6a7
akshat1423 <22b0690@iitb.ac.in>
2024-09-20 19:07:31 +0530

    Refactor Button component to remove unused CSS import

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import './Button.css';
+// import './Button.css';
 
 const Button = ({ icon, text }) => {
   return (
