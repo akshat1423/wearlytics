e684d3134e76e6fcfcf2584fd38d0311d9021661
akshat1423 <22b0690@iitb.ac.in>
2024-09-24 03:03:40 +0530

    authentication and ai voice assistant

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = ({ setUserId }) => {
     const handleLogin = async (e) => {
         e.preventDefault();
     
-        const response = await fetch('http://127.0.0.1:8000/api/login/', {
+        const response = await fetch('https://wearlytics.pythonanywhere.com//api/login/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
